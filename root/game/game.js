324530c48a6a3bbf7b8662149429f06cd00a7241
Lucas-kantola <0504luka@gapp.uddevalla.se>
2023-12-27 20:22:32 +0100

    Added sounds, animation and fixed bugs

--- root/game/game.js
+++ root/game/game.js
@@ -1,25 +1,36 @@
+//manu varaiables
 const radioButtons = document.querySelectorAll('input[name="number"]');
 const playButton = document.getElementById("playButton");
 const gameContainer = document.getElementById("gameCont")
 const buttonContainer = document.getElementById("buttonCont")
 const feedback = document.getElementById("feedback")
-const rollButton = document.getElementById("roll")
-const playAgainButton = document.getElementById("reset")
 
+//dice and game variables
 const dice1 = document.getElementById("dice1")
 const dice2 = document.getElementById("dice2")
 const scoreText = document.getElementById("score")
 const rollText = document.getElementById("dieValues")
+const rollButton = document.getElementById("roll")
+const playAgainButton = document.getElementById("reset")
 
+//values for the game
 var score = 0
+const rollDuration = 800; // Time in milliseconds for rolling animation
 
+//clock for later uses
 const timer = (ms) => new Promise(res => setTimeout(res, ms))
 
+//setting defaults 
 gameContainer.style.display = "none"
 playAgainButton.disabled = true;
 playAgainButton.style.display = "none"
 
+//defining defaults
 var selectedRadio;
+var rolling;
+var gameLost; 
+
+//reading the radio buttons
 playButton.addEventListener("click", () =>{
     for(const button of radioButtons){
         if(button.checked){
@@ -33,10 +44,6 @@ playButton.addEventListener("click", () =>{
     if(selectedRadio != null){
         gameContainer.style.display = "flex"
         buttonContainer.style.display = "none"
-
-        dieRoll(dice1)
-        dieRoll(dice2)
-
     }
     else{
         feedback.innerText = "Please select a number before pressing play"
@@ -45,31 +52,40 @@ playButton.addEventListener("click", () =>{
 
 })
 
+
+//rullar och kollar för förlust => display knockout
 rollButton.addEventListener("click", async () => {
-    let [value1, value2] = await Promise.all([dieRoll(dice1), dieRoll(dice2)])
-    value1 += 1
-    value2 += 1
-
-    score += value1 + value2
-    rollText.innerText = `Dice: (${value1}:${value2})`
-    scoreText.textContent = `Score: ${score} points`
-
-    if((value1 + value2) == selectedRadio.value){
-        rollButton.disabled = true;
-        playAgainButton.disabled = false;
-        const image = document.createElement("img")
-        image.src = "./images/knockout.png"
-        image.style.position = "absolute"
-        image.style.top = "50%"
-        image.style.left = "50%"
-        image.style.transform = "translate(-50%, -50%)"
-        document.body.appendChild(image)
-        
-        // Wait for 3 seconds before enabling the play again button
-        await timer(3000);
-        image.remove()
-        playAgainButton.style.display = "flex";
-        playAgainButton.addEventListener("click", resetGame);
+    if (!rolling) {     
+        let [value1, value2] = await Promise.all([dieRoll(dice1), dieRoll(dice2)])
+        value1 += 1
+        value2 += 1
+    
+        console.log(value1 + value2);
+        console.log(selectedRadio.value);
+        console.log((value1 + value2) == selectedRadio.value);
+        score += value1 + value2
+        rollText.innerText = `Dice: (${value1}:${value2})`
+        scoreText.textContent = `Score: ${score} points`
+    
+        if((value1 + value2) == selectedRadio.value || gameLost){
+            gameLost = true
+            rollButton.disabled = true;
+            playAgainButton.disabled = false;
+            await timer(500)
+            const image = document.createElement("img")
+            image.src = "./images/knockout.png"
+            image.style.position = "absolute"
+            image.style.top = "50%"
+            image.style.left = "50%"
+            image.style.transform = "translate(-50%, -50%)"
+            document.body.appendChild(image)
+            
+            // Wait for 3 seconds before enabling the play again button
+            await timer(3000);
+            image.remove()
+            playAgainButton.style.display = "flex";
+            playAgainButton.addEventListener("click", resetGame);
+        }
     }
 })
 
@@ -77,7 +93,9 @@ rollButton.addEventListener("click", async () => {
  * 
  * @param {Element} dice element to animate
  */
-async function dieRoll(dice){
+async function dieRoll(dice) {
+    rollButton.disabled = true
+    rolling = true
     const faces = [
         "./images/dice/d1.png",
         "./images/dice/d2.png",
@@ -85,35 +103,50 @@ async function dieRoll(dice){
         "./images/dice/d4.png",
         "./images/dice/d5.png",
         "./images/dice/d6.png"
-    ]    
+    ];
+    const sounds = [
+        "./sounds/1.mp3",
+        "./sounds/2.mp3"
+    ]
+    const rollSound = Math.floor(Math.random() * 2)
+    var audioPlayer = new Audio(sounds[rollSound])
+    audioPlayer.play()
 
-    const rollDuration = 800 //time in miliseconds
-    const frames = 50 // number of animation frames
+    // Randomize final faces
+    const finalFace = Math.floor(Math.random() * faces.length);
 
-    for(let i = 0; i < frames; i++){
-        const progress = i / (frames - 1)
-        const easing = Math.pow((1 - progress), 2)
+    dice.style.transition = 'transform 0.5s'; // Set a transition for smoother animation
 
-        const frameIndex = Math.floor(easing * (faces.length - 1))
-        const frame = faces[frameIndex]
+    for (let i = 0; i < 20; i++) { // Reduce frames for a smoother transition
+        const frameIndex = Math.floor(Math.random() * faces.length);
+        const frame = faces[frameIndex];
+        dice.src = frame;
 
-        dice.src = frame
+        const rotation = Math.random() * 30 - 15; // Random rotation within -10 to 10 degrees
+        const scale = 0.85 + Math.random() * 0.4; // Random scale between 0.9 to 1.1
 
-        const frameDuration = rollDuration / frames
+        dice.style.transform = `rotate(${rotation}deg) scale(${scale})`;
 
-        await timer(frameDuration)
+        await timer(rollDuration / 20); // Adjust timing for fewer frames
     }
 
-    const finalFace = Math.floor(Math.random() * faces.length)
-    dice.src = faces[finalFace] 
-    return finalFace
+    dice.src = faces[finalFace];
+    dice.style.transform = 'none'; // Reset transform
+
+    rolling = false;
+    rollButton.disabled = false
+    return finalFace;
 }
 
+/**
+ * Resets the games containers and buttons 
+ */
 function resetGame(){
     gameContainer.style.display = "none"
     buttonContainer.style.display = "flex"
     rollButton.disabled = false;
     playAgainButton.disabled = true;
     score = 0
+    gameLost = false
     scoreText.textContent = "Score: 0 points"
 }
